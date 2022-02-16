e1fb439d4050cbfd20b20241a78710a96a1be175
GrapeJuiceSoda <kendallcarmel@gmail.com>
2022-02-16 00:36:34 -0800

    font works

--- early-init.el
+++ early-init.el
@@ -1,17 +1,13 @@
-;; Emacs Color and Set Faces
-(set-face-background 'cursor "white smoke")
-(set-face-background 'mouse "white smoke")
-(set-face-background 'region  "#666")
-(when (member "Source Code Pro" (font-family-list))
-    (progn
-	  (setq default-frame-alist '((font . "Source Code Pro")))
-	  (set-face-attribute 'default nil :height 100))) ;  2022-02-13: The value is in 1/10pt, so 100 will give you 10pt, etc.
-(setq read-file-name-completion-ignore-case t)
-(setq frame-resize-pixelwise t)
-;;  (set-frame-parameter (selected-frame) 'alpha '(active . inactive))
-;;  (set-frame-parameter (selected-frame) 'alpha '(both))
-;;  (set-frame-parameter (selected-frame) 'alpha '(100 . 100))
-(add-to-list 'default-frame-alist '(alpha . (100)))
+;; This file is loaded before the package system and GUI is initialized, so in
+;; it you can customize variables that affect frame appearance as well as the
+;; package initialization process, such as package-enable-at-startup,
+;; package-load-list, and package-user-dir. Note that variables like
+;; package-archives which only affect the installation of new packages, and not
+;; the process of making already-installed packages available, may be customized
+;; in the regular init file.
+
+;; The value is in 1/10pt, so 100 will give you 10pt
+(set-face-attribute 'default nil :height 100)
 
 (defvar startup/file-name-handler-alist file-name-handler-alist)
 (setq file-name-handler-alist nil)
