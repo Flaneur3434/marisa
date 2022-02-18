1518f42b96a8bde5f504bc7d7422cd4babd6f3e0
GrapeJuiceSoda <kendallcarmel@gmail.com>
2022-02-17 22:59:43 -0800

    alias in eshell

--- init.el
+++ init.el
@@ -51,6 +51,7 @@
 (require 'rect-extension)				; TODO 2022-02-09: Add keybinds and functions
 (require 'screenshot)
 (require 'smart-tab)
+(require 'smooth-scroll)
 (require 'wc-mode)
 (require 'wgrep)
 
@@ -88,10 +89,6 @@
 
 (put 'narrow-to-region 'disabled nil)
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(display-line-numbers-widen t)
  '(package-selected-packages
    '(flycheck caml caml-mode sml-mode orderless corfu meghanada irony compnay mozc highlight auto-compplete expand-region which-key use-package undo-tree powerline pfuture page-break-lines magit ido-vertical-mode hydra htmlize ht goto-chg go-mode geiser-mit elisp-format dired-toggle-sudo diminish dashboard crux cfrs beacon auto-package-update async)))
