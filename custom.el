47462b87e86e48d9c64320de1428655b24c98b04
GrapeJuiceSoda <kendallcarmel@gmail.com>
2022-02-20 15:47:33 -0800

    some changes to c-mode and removed megahanda cause it always breaks >:(

--- custom.el
+++ custom.el
@@ -3,15 +3,20 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(c-default-style
+   '((c-mode . "OpenBSD")
+	 (java-mode . "Google")
+	 (awk-mode . "awk")
+	 (other . "Google")))
  '(display-line-numbers-widen t)
  '(package-selected-packages
-   '(flycheck caml caml-mode sml-mode orderless corfu meghanada irony compnay mozc highlight auto-compplete expand-region which-key use-package undo-tree powerline pfuture page-break-lines magit ido-vertical-mode hydra htmlize ht goto-chg go-mode geiser-mit elisp-format dired-toggle-sudo diminish dashboard crux cfrs beacon auto-package-update async)))
+   '(flycheck caml caml-mode sml-mode orderless corfu irony compnay mozc highlight auto-compplete expand-region which-key use-package undo-tree powerline pfuture page-break-lines magit ido-vertical-mode hydra htmlize ht goto-chg go-mode geiser-mit elisp-format dired-toggle-sudo diminish dashboard crux cfrs beacon auto-package-update async)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(fixed-pitch ((t ("Agave" :height 140))))
+ '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
  '(region ((t (:extend t :background "dim gray"))))
- '(variable-pitch ((t ("HackGenNerd" :height 140))))
- '(fixed-pitch ((t ( "Agave" :height 140))))
- '(org-indent ((t (:inherit (org-hide fixed-pitch))))))
+ '(variable-pitch ((t ("HackGenNerd" :height 140)))))
