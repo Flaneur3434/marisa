2ea1614661f6cb739503db3c425440771502aad1
GrapeJuiceSoda <kendallcarmel@gmail.com>
2022-02-27 01:17:52 -0800

    deleted ysnippt and company-mode config

--- custom.el
+++ custom.el
@@ -9,8 +9,9 @@
 	 (awk-mode . "awk")
 	 (other . "Google")))
  '(display-line-numbers-widen t)
+ '(lsp-java-theme "")
  '(package-selected-packages
-   '(yasnippet flycheck caml caml-mode sml-mode orderless corfu irony compnay mozc highlight auto-compplete expand-region which-key use-package undo-tree powerline pfuture page-break-lines magit ido-vertical-mode hydra htmlize ht goto-chg go-mode geiser-mit elisp-format dired-toggle-sudo diminish dashboard crux cfrs beacon auto-package-update async)))
+   '(flycheck caml caml-mode sml-mode orderless corfu irony compnay mozc highlight auto-compplete expand-region which-key use-package undo-tree powerline pfuture page-break-lines magit ido-vertical-mode hydra htmlize ht goto-chg go-mode geiser-mit elisp-format dired-toggle-sudo diminish dashboard crux cfrs beacon auto-package-update async)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
