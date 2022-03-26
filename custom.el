465880431a1d798bd0fd50fa8b45111e4c4deb16
GrapeJuiceSoda <kendallcarmel@gmail.com>
2022-03-26 14:15:24 -0700

    added bufler for project buffer management
    undo-hl duration changed
    
    fixed type-o

--- custom.el
+++ custom.el
@@ -12,14 +12,15 @@
  '(dumb-jump-selector 'popup)
  '(lsp-java-theme "")
  '(package-selected-packages
-   '(dumb-jump company-irony company caml caml-mode sml-mode orderless corfu irony compnay mozc highlight auto-compplete expand-region which-key use-package undo-tree powerline pfuture page-break-lines magit ido-vertical-mode hydra htmlize ht goto-chg go-mode geiser-mit dired-toggle-sudo diminish dashboard crux cfrs beacon auto-package-update async)))
+   '(bufler pretty-hydra f dumb-jump company-irony company caml caml-mode sml-mode orderless corfu irony compnay mozc highlight auto-compplete expand-region which-key use-package undo-tree powerline pfuture page-break-lines magit ido-vertical-mode hydra htmlize ht goto-chg go-mode geiser-mit dired-toggle-sudo diminish dashboard crux cfrs beacon auto-package-update async))
+ '(undo-hl-flash-duration 0.05))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(company-template-field ((t (:foreground "honeydew"))))
- '(cursor ((t (:background "black" :foreground "white"))))
+ '(cursor ((t nil)))
  '(fixed-pitch ((t ("Agave" :height 140))))
  '(highlight-changes ((t (:foreground "dim gray" :weight bold))))
  '(hl-line ((t (:extend nil :background "gray21"))))
--- lisp/mct-customizations.el
+++ lisp/mct-customizations.el
@@ -6,10 +6,11 @@
 (setq mct-minimum-input 3)
 (setq mct-live-update-delay 0.6)
 (setq mct-completions-format 'one-column)
-;; (mct-region-global-mode 1)
 (mct-minibuffer-mode 1)
 (setq completion-show-inline-help nil)
 (setq completions-detailed t)
 (setq resize-mini-windows t)
+
+(add-hook 'completion-list-mode-hook (lambda () (setq-local global-hl-line-mode nil)))
 
 (provide 'mct-customizations)
