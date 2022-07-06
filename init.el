a834845cbdafa09c23fa7efbc390e7d0cc190cc3
GrapeJuiceSoda <kendallcarmel@gmail.com>
2022-07-06 00:00:52 -0700

    fixed up minimal

--- init.el
+++ init.el
@@ -16,74 +16,20 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/monochrome-theme")
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/emacs-flatland-black-theme")
 
-(use-package aggressive-indent
-  :ensure t)
-
-(use-package dash
-  :ensure t)
-
-(use-package dired-toggle-sudo
-  :ensure t)
-
-(use-package exec-path-from-shell
-  :ensure t)
-
-(use-package expand-region
-  :ensure t)
-
-(use-package ido-vertical-mode
-  :ensure t)
-
-(use-package iedit
-  :ensure t)
-
-(use-package mct
-  :ensure t)
-
-(use-package pcmpl-args
-  :ensure t)
-
-(use-package page-break-lines
-  :ensure t)
-
-(use-package popwin
-  :ensure t)
-
-(use-package queue
-  :ensure t)
-
-(use-package s
-  :ensure t)
-
-(use-package undo-tree
-  :ensure t)
-
-(use-package undohist
-  :ensure t)
-
-(use-package which-key
-  :ensure t)
-
-
 (add-to-list 'load-path "~/.emacs.d/lisp")
 (add-to-list 'load-path "~/.emacs.d/lisp/prot-lisp")
 (add-to-list 'load-path "~/.emacs.d/lisp/wgrep")
 
 (require 'ag)
-(require 'aggressive-indent)
 (require 'basic-edit-toolkit)
 (require 'color)
 (require 'ctags-utils)
 (require 'darkroom)
 (require 'deferred)
 (require 'dired-sort)
-(require 'exec-path-from-shell)
 (require 'google-c-style)
 (require 'google-java-format)
 (require 'go-to-char)
-(require 'iedit)
-(require 'iedit-rect)
-(require 'inertial-scroll)
 (require 'ken_nc-custom-colors)
 (require 'ken_nc-eshell)
 (require 'ken_nc-functions)
@@ -91,16 +37,14 @@
 (require 'move-text)
 (require 'openbsd-knf-style)
 (require 'package)
-(require 'pcmpl-args)
-(require 'popwin)
 (require 'project)
 (require 'prot-comment)
 (require 'prot-common)
 (require 'prot-diff)
 (require 'prot-eshell)
 (require 'prot-simple)
+(require 'recentf)
 (require 'rect-extension)				; TODO 2022-02-09: Add keybinds and functions
-(require 'undohist)
 (require 'unfill)
 (require 'wc-mode)
 (require 'wgrep)
