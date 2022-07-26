3e0fe155c1fa9ea5207e1bf06214d0283003f5c2
GrapeJuiceSoda <kendallcarmel@gmail.com>
2022-07-25 22:22:39 -0700

    added python config

--- custom.el
+++ custom.el
@@ -12,11 +12,12 @@
  '(custom-enabled-themes '(xresources))
  '(custom-safe-themes
    '("cab317d0125d7aab145bc7ee03a1e16804d5abdfa2aa8738198ac30dc5f7b569" "39dd7106e6387e0c45dfce8ed44351078f6acd29a345d8b22e7b8e54ac25bac4" "acb636fb88d15c6dd4432e7f197600a67a48fd35b54e82ea435d7cd52620c96d" "9f6c33886ec2ef02f95e367f8f4f0177ecca36bf4d5d76ef2bbec59b2584bbcf" default))
+ '(dirvish-cache-dir "/home/ken_nc/.emacs.d/dirvish-cache/")
  '(display-line-numbers-widen t)
  '(dumb-jump-selector 'popup)
  '(lsp-java-theme "")
  '(package-selected-packages
-   '(company-irony-c-headers company-quickhelp yasnippet-snippets eglot bufler pretty-hydra f dumb-jump company-irony company caml caml-mode sml-mode orderless corfu irony compnay mozc highlight auto-compplete expand-region which-key use-package undo-tree powerline pfuture page-break-lines magit ido-vertical-mode hydra htmlize ht goto-chg go-mode geiser-mit dired-toggle-sudo diminish dashboard crux cfrs beacon auto-package-update async))
+   '(python-mode blacken pyvenv company-irony-c-headers company-quickhelp yasnippet-snippets eglot bufler pretty-hydra f dumb-jump company-irony company caml caml-mode sml-mode orderless corfu irony compnay mozc highlight auto-compplete expand-region which-key use-package undo-tree powerline pfuture page-break-lines magit ido-vertical-mode hydra htmlize ht goto-chg go-mode geiser-mit dired-toggle-sudo diminish dashboard crux cfrs beacon auto-package-update async))
  '(undo-hl-flash-duration 0.05))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
