6456eb8c2b5ea365d3b2840fcb1c4d976eb1a5c1
GrapeJuiceSoda <kendallcarmel@gmail.com>
2023-10-30 12:28:01 -0700

    no more json

---
+++ elpa/fussy-20230921.1850/fussy-pkg.el
@@ -0,0 +1,15 @@
+(define-package "fussy" "20230921.1850" "Fuzzy completion style using `flx'"
+  '((emacs "27.2")
+    (flx "0.5"))
+  :commit "2ff8317c403b15d3c898f7dcd8511b6d16f92c1d" :authors
+  '(("James Nguyen" . "james@jojojames.com"))
+  :maintainers
+  '(("James Nguyen" . "james@jojojames.com"))
+  :maintainer
+  '("James Nguyen" . "james@jojojames.com")
+  :keywords
+  '("matching")
+  :url "https://github.com/jojojames/fussy")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
