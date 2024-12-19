28ac0b1925fb2f3785fe8c42a2b72f1eda034782
John Doe <ken_nc@mikushock>
2024-12-18 22:03:42 -0800

    using xah-fly-key fork

--- etc/custom.el
+++ etc/custom.el
@@ -3,16 +3,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(pcmpl-args modern-cpp-font-lock flycheck dumb-jump aggressive-indent company-quickhelp company fancy-compilation vterm 0xc fussy hotfuzz embark-consult embark affe orderless consult-yasnippet consult vertico smart-hungry-delete exec-path-from-shell wrap-region popwin undo-fu-session iedit git-timemachine diff-hl pulsar projectile expand-region magit dired-toggle-sudo amx crux async beacon dashboard which-key gcmh wc-mode mwim xah-fly-keys blackout wgrep no-littering vc-use-package))
  '(package-vc-selected-packages
-   '((vc-use-package :vc-backend Git :url "https://github.com/slotThe/vc-use-package"))))
+   '((wc-mode :vc-backend Git :url "https://github.com/bnbeckwith/wc-mode")
+     (xah-fly-keys :vc-backend Git :url "https://github.com/Flaneur3434/xah-fly-keys")
+     (vc-use-package :vc-backend Git :url "https://github.com/slotThe/vc-use-package"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(cursor ((t (:background "steel blue"))))
- '(eglot-mode-line ((t (:weight normal))))
- '(highlight-changes ((t (:foreground "dim gray" :weight bold))))
- '(hl-line ((t nil)))
- '(region ((t (:extend t :background "#86817b" :foreground "black"))))
- '(which-func ((t (:foreground "orchid4")))))
+ )
